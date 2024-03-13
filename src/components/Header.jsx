67d129db4f82c04ae8b6bbad1123f066af15d6b0
Oguzcan Ayan <oguzcanayan43@hotmail.com>
2024-03-13 14:43:42 +0300

    update-project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,22 @@
 import React, { useState, useEffect, useRef } from 'react';
-import uSwappLogo from './uSwappLogo';
 import { IoMdSearch } from "react-icons/io";
+import ProjectLogo from './ProjectLogo';
+import { MdOutlineNotificationsActive } from "react-icons/md";
+import { IoSettingsOutline } from "react-icons/io5";
+import { LuUser2 } from "react-icons/lu";
+import { MdLogout } from "react-icons/md";
+import { IoIosArrowDown } from "react-icons/io";
+import { MdOutlineLightMode } from "react-icons/md";
+import { MdNightlightRound } from "react-icons/md";
 
 function Header() {
 
     const [isTabletMobile, setIsTabletMobile] = useState(false);
     const [search, setSearch] = useState("");
+    const [isFocused, setIsFocused] = useState(false);
+    const [langs, setLangs] = useState(false);
+    const [themes, setThemes] = useState(false);
+    const ref = useRef(null);
 
     useEffect(() => {
         const handleResize = () => {
@@ -21,14 +32,29 @@ function Header() {
 
     }, [])
 
+    useEffect(() => {
+        const handleClickOutsideLangBox = e => {
+            if (ref.current && !ref.current.contains(e.target)) {
+                setLangs(false);
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutsideLangBox);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutsideLangBox);
+        }
+
+    }, [ref])
+
     return (
         <>
             <header>
                 <div className='uswapp-header'>
 
-                    <uSwappLogo />
+                    <ProjectLogo />
 
-                    <div className='searchbox'>
+                    <div className={`searchbox ${isFocused ? 'active' : ''}`}>
                         <span>
                             <IoMdSearch />
                         </span>
@@ -37,6 +63,8 @@ function Header() {
                             placeholder='Search a product...'
                             value={search}
                             onChange={(e) => setSearch(e.target.value)}
+                            onFocus={() => {setIsFocused(true)}}
+                            onBlur={() => {setIsFocused(false)}}
                         />
                     </div>
 
@@ -45,32 +73,36 @@ function Header() {
                     </div>
 
                     <div className='lang-content'>
-                        <button>EN</button>
-                        <button>TR</button>
+                        <span onClick={() => {setLangs(!langs)}}>
+                            {langs}
+                            <IoIosArrowDown />
+                        </span>
+                        {langs ? <button value="TR">TR</button> : <button value="EN">EN</button>}
                     </div>
                     <div className='theme-content'>
-                        <button>Light</button>
-                        <button>Dark</button>
+                        <span onClick={() => {setThemes(!themes)}}>
+                            
+                        </span>
                     </div>
                     <div className='user-contents'>
                         <div className='notifications'>
-                            <span>icon</span>
+                            <span><MdOutlineNotificationsActive /></span>
                             <span>Notifications</span>
                         </div>
                         <div className='settings'>
-                            <span>icon</span>
+                            <span><IoSettingsOutline /></span>
                             <span>Settings</span>
                         </div>
                         <div className='user-log'>
                             <div className='account'>
-                                <span>icon</span>
+                                <span><LuUser2 /></span>
                                 <span>username</span>
                                 <span>user-mail</span>
                             </div>
-                            <div className='log-out'>
-                                <span>icon</span>
-                                <span>logout</span>
-                            </div>
+                        </div>
+                        <div className='log-out'>
+                            <span><MdLogout /></span>
+                            <span>logout</span>
                         </div>
                     </div>
                 </div>
---
+++ src/components/ProjectLogo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ProjectLogo() {
+  return (
+    <>
+        <div className='uswapp-logo'>
+            <img src="../public/assets/images/uswapp-logo.svg" alt="uSwapp-logo" />
+        </div>
+    
+    </>
+  )
+}
+
+export default ProjectLogo;
