498b954b87fcdd5e37fd08d77ca3eb863c1cbd00
Oguzcan Ayan <oguzcanayan43@hotmail.com>
2024-05-24 09:56:29 +0300

    project-router

--- src/Tools/Router/Router.jsx
+++ src/Tools/Router/Router.jsx
@@ -1,12 +1,71 @@
 import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '../../Layouts/MainLayout';
+import ErrorPage from '../../pages/ErrorPage';
+import HomePageLayout from '../../Layouts/HomePageLayout';
+import ProductAddLayout from '../../Layouts/ProductAddLayout';
+import ProductAddPage from '../../pages/ProductAddPage';
+import ProductViewLayout from '../../Layouts/ProductViewLayout';
+import ProductViewPage from '../../pages/ProductViewPage';
+import UserProfileLayout from '../../Layouts/UserProfileLayout';
+import UserProfile from '../../pages/UserProfile';
 
-function Router() {
-  return (
-    <>
-    
-    
-    </>
-  )
-}
 
-export default Router;
+export const router = [
+
+  {
+    name: "Homepage",
+    link: "/",
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    breadcrumb: "Homepage",
+    children: [
+      {
+        index: true,
+        element: <HomePageLayout />,
+      },
+      {
+        name: "Product Add",
+        link: "/product-add",
+        path: "product-add",
+        element: <ProductAddLayout />,
+        breadcrumb: "Product Add",
+        children: [
+          {
+            index: true,
+            element: <ProductAddPage />
+          },
+          {
+            name: "Product View",
+            link: "/product-add/product-view",
+            path: "product-view",
+            element: <ProductViewLayout />,
+            breadcrumb: "Product View",
+            children: [
+              {
+                index: true,
+                element: <ProductViewPage />
+              },
+              {
+                name: "User Profile",
+                link: "/product-add/product-view/user-profile",
+                path: "user-profile",
+                element: <UserProfileLayout />,
+                breadcrumb: "User Profile",
+                children: [
+                  {
+                    index: true,
+                    element: <UserProfile />
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+]
+
+export default createBrowserRouter(router);
