157b51def8c6f9f0eaab0851997f374bbc7989c1
Oguzcan Ayan <oguzcanayan43@hotmail.com>
2024-05-26 15:51:02 +0300

    kinde-auth

---
+++ src/Tools/Auth/Auth.jsx
@@ -0,0 +1,12 @@
+/* import { KindeProvider } from "@kinde-oss/kinde-auth-react";
+
+const auth = () => (
+    <KindeProvider
+        clientId="3b8ecb9afb9445bda7ba2b9f2af53c2c"
+        domain="https://uswapp.kinde.com"
+        redirectUri="http://localhost:5173"
+        logoutUri="http://localhost:5173"
+    >
+        <Routes />
+    </KindeProvider>
+); */
